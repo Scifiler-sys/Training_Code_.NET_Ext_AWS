fb995757b63b6582f6431833680950465166f0a0
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-07 16:39:02 -0500

    Added Restaurant class

--- 1 - csharp/RestaurantReview/RRModels/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RRModels
-{
-    public class Class1
-    {
-    }
-}
---
+++ 1 - csharp/RestaurantReview/RRModels/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+    This RRModels namespace is responsible for holding the data structure of our application
+*/
+namespace RRModels
+{
+    public class Restaurant
+    {
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        
+    }
+}
