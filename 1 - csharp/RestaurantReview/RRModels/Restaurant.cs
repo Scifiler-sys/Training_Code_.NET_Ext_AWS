e9a383f2f32cdc9cd6a1e96cbc320a743d21b034
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-09 10:43:51 -0500

    Added barebones

--- 1 - csharp/RestaurantReview/RRModels/Restaurant.cs
+++ 1 - csharp/RestaurantReview/RRModels/Restaurant.cs
@@ -8,11 +8,23 @@ namespace RRModels
     public class Restaurant
     {
         private string _city;
+
+        public Restaurant()
+        { }
+
+        //The benefit of this is adding restrictions to how you want this field to be set.
+        //You can also manipulate how this field will be get
         public string City
         {
-            get { return _city; }
-            set { _city = value; }
+            get { return $"This restaurant is at {_city}"; }
+            set { _city = value.ToLower(); }
         }
         
+        //You can just create stand alone properties
+        public string State { get; set; }
+
+        public int Id { get; set; }
+        
+
     }
 }
---
+++ 1 - csharp/RestaurantReview/RRUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RRModels;
+
+namespace RRUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //We can set our property values this way (kinda makes constructor overloading not needed)
+            Restaurant rest = new Restaurant()
+            {
+                City = "Houston",
+                State = "Texas"
+            };
+            Console.WriteLine(rest.City);
+            
+        }
+    }
+}
