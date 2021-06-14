c05e2c4dd55f7332487839ffc040d3a7a4af9f46
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-14 11:15:17 -0500

    Updated to have exception handling

--- 1-csharp/HelloWorld/Serialization.cs
+++ 1-csharp/HelloWorld/Serialization.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
     public class Serialization
     {
         private const string _filePath = "./StoredFile/House.json";
-        public static void SerialMain()
+        public static void SerialMain() 
         {
             //Created house object to be serialized
             House house1 = new House()
@@ -29,15 +29,23 @@ namespace HelloWorld
             //Deserialize the object we just stored
             try
             {
-                jsonString = File.ReadAllText(_filePath);
+                //ReadAllText method might throw an error if it isn't able to find the filepath
+                //We will handle it 
+                jsonString = File.ReadAllText(_filePath+ "sedf");//+"askljhfe");
                 House house2 = JsonSerializer.Deserialize<House>(jsonString);
                 Console.WriteLine(house2);
+                // throw new Exception("unknown error");
             }
-            catch (FileNotFoundException e)
+            catch (FileNotFoundException)
             {
                 //Will catch the error if the file can't be found by the given filePath
-                Console.WriteLine(e);
-                throw;
+                //Console.WriteLine(e);
+                Console.WriteLine("The file could not be found. Please give the correct file path");
+                // throw new Exception("The file could not be found. Please give the correct file path");
+            }
+            catch(Exception) //It is the base class that all exception classes inherits from
+            {
+                Console.WriteLine("Code went through some unknown error");
             }
         }
     }
