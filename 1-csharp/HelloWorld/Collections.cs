6d0d6de57777c42754a8b8021f7cf7977425767e
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-14 10:55:14 -0500

    Added comments

--- 1-csharp/HelloWorld/Collections.cs
+++ 1-csharp/HelloWorld/Collections.cs
@@ -14,16 +14,20 @@ public class Collections
     {
         Console.WriteLine("====Collections Tests====");
         Console.WriteLine("=List Demo=");
+        //We are adding elements to the strings List
         strings.Add("First element");
         strings.Add("Second element");
         strings.Add("Third element");
 
         //A way to go through a list
+        //Foreach will iterate through all of the elements of collections
         foreach (string element in strings)
         {
             Console.WriteLine(element);
         }
 
+        //for is a more detail way to iterate through the elements of collection
+        //You control how to iterate through
         for (int i = 0; i < strings.Count; i++)
         {
             Console.WriteLine("Current Index: " + i);
@@ -31,12 +35,14 @@ public class Collections
         }
 
         int x = 0;
+        //Will keep iterating until the boolean expression inside the while loop returns false
         while (x< strings.Count)
         {
             Console.WriteLine(strings[x++]);
         }
         
         Console.WriteLine("=ArrayList Demo=");
+        //We can add different types in the ArrayList
         arrayLists.Add("First element");
         arrayLists.Add(2);
         arrayLists.Add(3.5);
