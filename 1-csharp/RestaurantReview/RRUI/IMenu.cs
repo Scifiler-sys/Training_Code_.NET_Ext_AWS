d555372bdd4983eb4f5a2964efeb926546bbba54
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-14 13:29:40 -0500

    Added xml documentation

--- 1-csharp/RestaurantReview/RRUI/IMenu.cs
+++ 1-csharp/RestaurantReview/RRUI/IMenu.cs
@@ -13,8 +13,15 @@ namespace RRUI
     public interface IMenu
     { 
         //as an interface, it will automatically fill in public (access-modifier) and abstract (non-access modifier)
+        /// <summary>
+        /// Will display the overall menu of the project and the choices you can make
+        /// </summary>
         void Menu();
 
+        /// <summary>
+        /// This method will record the user's choice and change your menu based on the end-user's choice
+        /// </summary>
+        /// <returns> Returns a MenuType that the user picked</returns>
         MenuType YourChoice();
     }
 }
