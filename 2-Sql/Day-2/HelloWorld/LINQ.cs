005846b0d9a7c7f00a81766697a7ba9252a0dc0e
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-16 17:39:31 -0500

    Added Linq functionality

--- 2-Sql/Day-2/HelloWorld/LINQ.cs
+++ 2-Sql/Day-2/HelloWorld/LINQ.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace HelloWorld
 {
@@ -6,7 +7,21 @@ namespace HelloWorld
     {
         public static void LINQMain()
         {
-            
+            //A data source
+            int[] numbers = new int[5]{0,1,2,3,4};
+
+            //Query that will select only even numbers
+            //Example of Query syntax
+            var queryResult = (from num in numbers
+                                where num%2 == 0
+                                select num);
+
+            //Execution
+            foreach (var item in queryResult)
+            {
+                Console.WriteLine(item);
+            }
+                            
         }
     }
 }
