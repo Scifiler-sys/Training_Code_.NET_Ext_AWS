a48af37b529d6d99b445d224d35f900a3974d149
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-22 10:24:23 -0500

    Add unit test

---
+++ 2-Sql/Day-3/RestaurantReview/RRTests/RepoTest.cs
@@ -0,0 +1,14 @@
+namespace RRTests
+{
+    public class RepoTest
+    {
+        /*
+            Unit tesing in DB needs Microsoft.EntityFrameworkCore.Sqlite package
+                                    Microsoft.EntityFrameworkCore.Design package
+            Sqlite has features that allows us to create an inmemory rdb (relational database) to test our DB
+
+        */
+
+        private readonly DBContextOptions<SPDBContext> options;
+    }
+}
