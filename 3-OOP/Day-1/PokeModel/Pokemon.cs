8c0b1895bbc9a52bc72782166354392938f134bb
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-04 13:11:11 -0500

    Added basic project structure

---
+++ 3-OOP/Day-1/PokeModel/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿namespace PokeModel
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+            
+        }
+
+        public string Name { get; set; }
+        public int PokeId { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+    }
+}
