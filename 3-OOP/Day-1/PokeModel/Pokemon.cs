2ef47eefa7dabf4a88d3b9ee1e236979a74e267b
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-04 17:59:50 -0500

    Added models

--- 3-OOP/Day-1/PokeModel/Pokemon.cs
+++ 3-OOP/Day-1/PokeModel/Pokemon.cs
@@ -4,13 +4,41 @@
     {
         public Pokemon()
         {
-            
+            this.Name = "Ditto";
+            this.Attack = 55;
+            this.Health = 55;
+            this.Defense = 55;
+            this._abilities = new List<Ability>(){new Ability()};
         }
 
         public string Name { get; set; }
-        public int PokeId { get; set; }
         public int Health { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
+        private List<Ability> _abilities;
+        public List<Ability> Abilities
+        {
+            get { return _abilities; }
+            set 
+            { 
+                if (value.Count < 5 && value.Count > 0)
+                {
+                    _abilities = value;
+                }
+                else if (value.Count == 0)
+                {
+                    throw new Exception("A pokemon must have 1 ability");
+                }
+                else
+                {
+                    throw new Exception("A pokemon can only have 4 abilities");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"===Pokemon Info===\nName: {Name}\nHealth: {Health}\nAttack: {Attack}\nDefense: {Defense}";
+        }
     }
 }
