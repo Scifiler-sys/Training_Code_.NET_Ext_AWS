ac19217f4c108f971e5e76460014570f7ee63db4
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-04 17:59:58 -0500

    Added abstraction

--- 3-OOP/Day-1/PokeUI/Program.cs
+++ 3-OOP/Day-1/PokeUI/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PokeModel;
+
+//Checking if creating out models is working
+Pokemon poke1 = new Pokemon();
+Console.WriteLine(poke1);
+Console.WriteLine(poke1.Abilities[0]);
+// List<Ability> abilityList = new List<Ability>();
+// poke1.Abilities = abilityList; //Our validation is working
+
+Ability ability1 = new Ability();
+// ability1.PP = 0; //Our validation is working
+Console.WriteLine(ability1);
