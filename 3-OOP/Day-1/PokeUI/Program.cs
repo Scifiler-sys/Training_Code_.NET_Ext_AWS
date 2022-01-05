4765aa3a6a313c0da79c08f7e1fb8747cb5e9acc
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-05 16:40:57 -0500

    Uncomment

--- 3-OOP/Day-1/PokeUI/Program.cs
+++ 3-OOP/Day-1/PokeUI/Program.cs
@@ -20,35 +20,35 @@
 // Console.WriteLine("Defend value: " + bulbasaur.DefendMove()); //Grasstype overrides the defendmove from pokemon class
 // Console.WriteLine("Defend value: " + bulbasaur.DefendMove(300)); //Grasstype overloads the defendmove to also take in a integer parameter
 
-// using PokeUI;
+using PokeUI;
 
-// bool repeat = true;
-// IMenu menu = new MainMenu();
+bool repeat = true;
+IMenu menu = new MainMenu();
 
-// //Will repeat the menu so it doesn't stop abruptly
-// while (repeat)
-// {
+//Will repeat the menu so it doesn't stop abruptly
+while (repeat)
+{
 
-//     Console.Clear(); //Clears terminals
-//     menu.Display(); //Displays menu
-//     string ans = menu.UserChoice(); //Ask for user choice
+    Console.Clear(); //Clears terminals
+    menu.Display(); //Displays menu
+    string ans = menu.UserChoice(); //Ask for user choice
 
-//     //Main place where we change our pages
-//     switch (ans)
-//     {
-//         case "Exit":
-//             repeat = false;
-//             break;
-//         case "AddPokemon":
-//             menu = new AddPokemon();
-//             break;
-//         case "MainMenu":
-//             menu = new MainMenu();
-//             break;
-//         default:
-//             Console.WriteLine("Page does not exist");
-//             Console.WriteLine("Please press Enter to continue");
-//             Console.ReadLine();
-//             break;
-//     }
-// }
+    //Main place where we change our pages
+    switch (ans)
+    {
+        case "Exit":
+            repeat = false;
+            break;
+        case "AddPokemon":
+            menu = new AddPokemon();
+            break;
+        case "MainMenu":
+            menu = new MainMenu();
+            break;
+        default:
+            Console.WriteLine("Page does not exist");
+            Console.WriteLine("Please press Enter to continue");
+            Console.ReadLine();
+            break;
+    }
+}
