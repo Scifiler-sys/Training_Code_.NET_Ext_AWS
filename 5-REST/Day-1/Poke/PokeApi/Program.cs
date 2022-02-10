0e74823204f96620f54fa78fc83cbe5c184ffbae
Scifiler-sys <pagdilaostephen@gmail.com>
2022-02-10 14:55:19 -0500

    Added autogen and controller

---
+++ 5-REST/Day-1/Poke/PokeApi/Program.cs
@@ -0,0 +1,31 @@
+using PokeBL;
+using PokeDL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository>(repo => new SQLRepository(builder.Configuration.GetConnectionString("Reference2DB")));
+builder.Services.AddScoped<IPokemonBL, PokemonBL>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
