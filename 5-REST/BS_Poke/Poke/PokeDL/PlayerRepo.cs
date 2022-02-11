3b727af08b73dc475ba93cd2d12d30e343ab16ca
Scifiler-sys <pagdilaostephen@gmail.com>
2022-02-11 12:12:45 -0500

    Added playerRepo

---
+++ 5-REST/BS_Poke/Poke/PokeDL/PlayerRepo.cs
@@ -0,0 +1,25 @@
+namespace PokeDL
+{
+    public class PlayerRepo : IRepository<Player>
+    {
+        public Player Add(Player p_resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player Delete(Player p_resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Player> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player Update(Player p_resource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
